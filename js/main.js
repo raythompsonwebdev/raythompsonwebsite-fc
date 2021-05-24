94802b97338f53685d7bb2bbe32ef0c43746d7f7
raythompsonwebdev <ray_thomp@hushmail.com>
2021-05-24 01:20:26 +0100

    added page scrolling using javascipt

--- js/main.js
+++ js/main.js
@@ -1,28 +1,73 @@
 "use strict";
 
-var openPage = function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default. */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+//Your code to run since DOM is loaded and ready
+document.addEventListener("DOMContentLoaded", function () {
+  // Add smooth scrolling to all links
+  var navLinks = document.getElementsByClassName("tablink"); // get body and html elements
 
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  } // Remove the background color of all tablinks/buttons
+  var scrollScreen = document.querySelector(["body", "html"]);
+  var siteHeader = document.querySelector(".site_header"); //In JavaScript, using the addEventListener() method:  object.addEventListener("scroll", myScript);
+
+  window.onscroll = function () {
+    if (scrollScreen.scrollTop >= 100) {
+      siteHeader.classList.add("fixed-header");
+      /* eslint-disable no-console */
+
+      console.log("done");
+      /* eslint-enabe no-console */
+    } else {
+      siteHeader.classList.remove("fixed-header");
+      /* eslint-disable no-console */
+
+      /* eslint-enabe no-console */
+    }
+  }; // scrolling function - (https://codepen.io/ugg0t/pen/mqBBBY)
 
 
-  tablinks = document.getElementsByClassName("tablink");
+  var scrollTo = function scrollTo(element) {
+    scrollScreen.scrollTo({
+      behavior: "smooth",
+      left: 0,
+      top: element.offsetTop - 110 // deduct height of header.
 
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  } // Show the specific tab content
-
-
-  document.getElementById(pageName).style.display = "block"; // Add the specific color to the button used to open the tab content
-
-  elmnt.style.backgroundColor = color;
-}; // Get the element with id="defaultOpen" and click on it
+    });
+  }; // turn HTML collection list of objects into an array
+  // Iterated over array with forEach.
 
 
-document.getElementById("defaultOpen").click();
-openPage;
+  Array.from(navLinks).forEach(function (link) {
+    //add event listener to each link
+    link.addEventListener("click", function (event) {
+      // Store hash
+      var hash = event.target.hash; //check if has empty
+
+      if (hash !== "") {
+        // if not, Prevent default anchor click behavior
+        event.preventDefault();
+        /* eslint-disable no-console */
+
+        console.log(hash, location.href);
+        /* eslint-enabe no-console */
+        // select element id converting hash to string using template literal and use as argument in scrolling function.
+
+        scrollTo(document.querySelector("".concat(hash)));
+      } // End if
+
+    });
+  });
+}); //      /* JavaScript */
+//      element.style.transition = 'margin-left 2s';
+//      element.addEventListener('transitionend',
+//        (event) => {  //When animation is finished });
+//      element.style.marginLeft = '100px';
+// $("button#myBtn").on("click", function (event) {
+//   $("html,body").animate({ scrollTop: 0 }, 800);
+// });
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("myBtn").style.display = "block";
+//   } else {
+//     document.getElementById("myBtn").style.display = "none";
+//   }
+// }
 //# sourceMappingURL=main.js.map
