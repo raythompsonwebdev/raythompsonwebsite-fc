3730e1da767cd7386d1b6fd29a20699fe56b6e5d
raythompsonwebdev <ray_thomp@hushmail.com>
2021-08-02 22:07:01 +0100

    using HTML5 API validation object to confirm validity of form inputs name email website and comments-social media links and other contact details to be added

--- js/dev/contact.js
+++ js/dev/contact.js
@@ -4,18 +4,18 @@ const error = document.getElementById("formerror");
 
 // deconstruct array of form elements
 //subscribed, select, comtype1, comtype2, comtype3 variable replaced with space g
-const [text, email, url, , , , , ,  ] = myForm;
+const [text, email, url, , , , , ,comment  ] = myForm;
 
-//var formData = new FormData(myForm);
 error.style.display = "none";
+
+//var formData = new FormData(my
 console.log(myForm);
 
-
-
 function validateForm(e) {
+
   e.preventDefault();
 
-  if (email.validity.valueMissing && text.validity.valueMissing) {
+  if (email.validity.valueMissing && text.validity.valueMissing ) {
     error.style.display = "block";
     error.textContent = "Fields cannot be empty.";
   } else {
@@ -30,13 +30,11 @@ function validateForm(e) {
     text.setCustomValidity("");
 
   }
-
   if (email.validity.typeMismatch || email.validity.patternMismatch) {
     email.setCustomValidity("I am expecting an e-mail address!");
 
   } else {
     email.setCustomValidity("");
-
   }
 
   if (url.validity.patternMismatch || url.validity.typeMismatch) {
@@ -44,12 +42,19 @@ function validateForm(e) {
 
   } else {
     url.setCustomValidity("");
+  }
 
+  if (comment.validity.tooShort || comment.validity.tooLong ) {
+    comment.setCustomValidity("I am expecting a valid text, No HTML!");
+
+  } else {
+    comment.setCustomValidity("");
   }
 
   email.reportValidity("");
   text.reportValidity("");
   url.reportValidity("");
+  comment.reportValidity("");
 }
 
 mySubmit.addEventListener("click", validateForm);
