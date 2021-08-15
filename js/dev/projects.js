4dc41c7b3f3f7e1416abac114a6bbdce11ed633d
raythompsonwebdev <ray_thomp@hushmail.com>
2021-08-15 19:56:56 +0100

    started protfolio grid on project page

---
+++ js/dev/projects.js
@@ -0,0 +1,25 @@
+const boxes = document.querySelectorAll(".box");
+const tabs = document.querySelectorAll(".project_tab");
+
+const arrayTabs = Array.from(tabs);
+const arrayBoxes = Array.from(boxes);
+
+console.log(arrayTabs);
+console.log(arrayBoxes);
+
+arrayTabs.forEach((element1) => {
+	// console.log(element1);
+	element1.addEventListener("click", (e) => {
+		e.preventDefault();
+		arrayBoxes.filter((element2) => {
+			// console.log(element2.dataset.id == element1.id);
+			if (element2.dataset.id === element1.id) {
+				// eslint  - "no-param-reassign": "off"
+				element2.style.display = "block";
+			} else {
+				element2.style.display = "none";
+			}
+			return element2;
+		});
+	});
+});
