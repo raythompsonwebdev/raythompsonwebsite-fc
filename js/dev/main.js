fa47bb79e0995ce207915eef94d8b2286876b413
raythompsonwebdev <ray_thomp@hushmail.com>
2021-10-12 20:04:50 +0100

    trying out an image slider and bar chart on about page

--- js/dev/main.js
+++ js/dev/main.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// get header element
 	const siteHeader = document.querySelector(".site_header");
-	const menuToggle = document.querySelector(".menu-toggle");
 
 	// get all divs with class tab content.
 	const scrollElements = document.querySelectorAll(".tabcontent");
@@ -72,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	/**
 	 * Mobile menu
 	 */
-
+	const menuToggle = document.querySelector(".menu-toggle");
 	// create menu variables
 	const slideoutMenu = document.querySelector(".site_navigation");
 	const slideoutMenuHeight = slideoutMenu.offsetHeight;
@@ -119,127 +118,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			} else {
 				slideoutMenu.style.top = "0px";
 			}
-		});
-	});
-
-	/**
-	 * Contact Form
-	 */
-	const myForm = document.forms[0];
-	const mySubmit = document.getElementById("submit");
-	const error = document.getElementById("form-error");
-	// deconstruct array of form elements
-	// subscribed, select, comtype1, comtype2, comtype3 variable replaced with space g
-	const [text, email, url, , , , , ,] = myForm;
-
-	error.classList.add("hide-error");
-	error.textContent = "";
-
-	text.addEventListener("blur", (e) => {
-		e.preventDefault();
-		text.style.setProperty("--text-error", "none");
-		if (text.validity.patternMismatch) {
-			error.classList.remove("hide-error");
-			error.classList.add("show-error");
-			error.textContent = "name too short or more than 40 characters. ";
-			text.style.setProperty("--text-error", "solid 2px #f38383cb");
-		} else {
-			error.classList.add("hide-error");
-			error.classList.remove("show-error");
-			error.textContent = "";
-			text.style.setProperty("--text-error", "none");
-		}
-	});
-
-	email.addEventListener("blur", (e) => {
-		e.preventDefault();
-		error.textContent = "";
-		email.style.setProperty("--email-error", "none");
-		if (email.validity.typeMismatch) {
-			error.classList.add("show-error");
-			error.classList.remove("hide-error");
-			error.textContent = "I am expecting an e-mail address!";
-			email.style.setProperty("--email-error", "solid 2px #f38383cb");
-		} else if (email.validity.patternMismatch) {
-			error.classList.add("show-error");
-			error.classList.remove("hide-error");
-			error.textContent = "I am expecting a valid e-mail address!";
-			email.style.setProperty("--email-error", "solid 2px rgb(250, 250, 135)");
-		} else {
-			error.classList.remove("show-error");
-			error.classList.add("hide-error");
-			error.textContent = "";
-			email.style.setProperty("--email-error", "none");
-		}
-	});
-
-	url.addEventListener("blur", (e) => {
-		e.preventDefault();
-		url.style.setProperty("--url-error", "none");
-		if (url.validity.typeMismatch) {
-			error.classList.remove("hide-error");
-			error.classList.add("show-error");
-			error.textContent = "I am expecting a web address!";
-			url.style.setProperty("--url-error", "solid 2px #f38383cb");
-		} else if (url.validity.patternMismatch) {
-			error.classList.remove("hide-error");
-			error.classList.add("show-error");
-			error.textContent = "I am expecting an valid web pattern!";
-			url.style.setProperty("--url-error", "solid 2px rgb(250, 250, 135)");
-		} else {
-			error.classList.remove("show-error");
-			error.classList.add("hide-error");
-			error.textContent = "";
-			url.style.setProperty("--url-error", "none");
-		}
-	});
-
-	function showError() {
-		if (text.validity.valueMissing && email.validity.valueMissing) {
-			error.classList.add("show-error");
-			error.classList.remove("hide-error");
-			error.textContent = "name and email address are required!";
-			text.style.setProperty("--text-error", "solid 2px rgb(136, 136, 241)");
-			email.style.setProperty("--email-error", "solid 2px rgb(136, 136, 241)");
-		} else if (!text.validity.valid && !email.validity.valid) {
-			error.classList.add("show-error");
-			error.classList.remove("hide-error");
-			error.textContent =
-				"name and email address are required and need to be valid!";
-			text.style.setProperty("--text-error", "solid 2px rgb(136, 136, 241)");
-			email.style.setProperty("--email-error", "solid 2px rgb(136, 136, 241)");
-		} else {
-			myForm.submit();
-		}
-	}
-	mySubmit.addEventListener("click", showError);
-
-	/**
-	 * project page portfolio grid
-	 */
-
-	const boxes = document.querySelectorAll(".box");
-	const tabs = document.querySelectorAll(".project_tab");
-	const arrayTabs = Array.from(tabs);
-	const arrayBoxes = Array.from(boxes);
-	arrayTabs.forEach((element1) => {
-		element1.addEventListener("click", (e) => {
-			e.preventDefault();
-
-			arrayBoxes.filter((element2) => {
-				if (element2.dataset.all === element1.id) {
-					// eslint  - "no-param-reassign": "off"
-					element2.classList.remove("box-hide");
-					element2.classList.add("box-show");
-				} else if (element2.dataset.id === element1.id) {
-					element2.classList.remove("box-hide");
-					element2.classList.add("box-show");
-				} else {
-					element2.classList.remove("box-show");
-					element2.classList.add("box-hide");
-				}
-				return element2;
-			});
 		});
 	});
 });
