5c418b7a406917936c9f0e6fd7b4c3597dfaaffe
raythompsonwebdev <ray_thomp@hushmail.com>
2021-12-02 23:38:33 +0000

    optimised images -update css - removes fontawesome cdn link

--- src/index.js
+++ src/index.js
@@ -6,84 +6,77 @@ import "./js/projects";
 import "./js/slider";
 import "./js/contact";
 
-import shoestorePic from "./images/Projects/www-shoestore-website-home-page.png";
-import travelagencyPic from "./images/Projects/travel-agency-home-page.png";
-import manneringPic from "./images/Projects/mannering-musicmvc-home-page.png";
-import quoteGeneratorPic from "./images/Projects/codepen-random-quote-generator.png";
-import productPagePic from "./images/Projects/codepen-scrolling-product-page.png";
-import cssDocPic from "./images/Projects/codepen-css-documentation-page.png";
-import weatherAppPic from "./images/Projects/code-pen-weather-app.png";
-import tributePagePic from "./images/Projects/codepen-obama-tribute-page.png";
-import clashvibesPic from "./images/Projects/www-clashvibes-website-home-page.png";
-import drumMachinePic from "./images/Projects/codepen-drum-machine-2021.10.26.png";
-import reactDrumMachinePic from "./images/Projects/codepen-react-weather-app-2021.10.26.png";
-import reactWeatherAppPic from "./images/Projects/codepen-react-random-quote-generator-2021.10.26.png";
-
+// set backround image for home page
 const bgImage = document.querySelector("#banner");
-
 bgImage.style.setProperty(
   "--bg-image",
   "url(images/sergi-kabrera-2xU7rYxsTiM-unsplash.jpg)"
 );
 
-// const profileImage = document.querySelector("#pageImage").srcset;
-
-// eslint-disable-next-line no-console
-// console.log(profileImage);
-
+// set alt attributes for portfolio grid images
 const shoeStore = document.getElementById("shoestoreImg");
-shoeStore.src = shoestorePic;
-shoeStore.setAttribute("alt", "Image of shoestore website home page");
+shoeStore.setAttribute("alt", "shoestore-home-page");
 
 const travelagency = document.getElementById("travelAgencyImg");
-travelagency.src = travelagencyPic;
-travelagency.setAttribute("alt", "Image of travel Agency website home page");
+travelagency.setAttribute("alt", "travel-agency-home-page");
 
 const mannering = document.getElementById("manneringImg");
-mannering.src = manneringPic;
-mannering.setAttribute(
-  "alt",
-  "Image of mannering music store website home page"
-);
+mannering.setAttribute("alt", "mannering-musicmvc-home-page");
 
 const quoteGenerator = document.getElementById("quoteGeneratorImg");
-quoteGenerator.src = quoteGeneratorPic;
-quoteGenerator.setAttribute(
-  "alt",
-  "Image of Javascript Random Quote Generator home page"
-);
+quoteGenerator.setAttribute("alt", "random-quote-generator");
 
 const productPage = document.getElementById("scrollingPageImg");
-productPage.src = productPagePic;
-productPage.setAttribute(
-  "alt",
-  "Image of scrolling product page website home page"
-);
+productPage.setAttribute("alt", "scrolling-product-page");
 
 const cssDoc = document.getElementById("cssDocImg");
-cssDoc.src = cssDocPic;
-cssDoc.setAttribute("alt", "Image of css documentation website home page");
+cssDoc.setAttribute("alt", "css-documentation-page");
 
 const weatherApp = document.getElementById("weatherAppImg");
-weatherApp.src = weatherAppPic;
-weatherApp.setAttribute("alt", "Image of Javascript Weather App home page");
+weatherApp.setAttribute("alt", "code-pen-weather-app");
 
 const tributePage = document.getElementById("tributePageImg");
-tributePage.src = tributePagePic;
-tributePage.setAttribute("alt", "Image of Barack Obama tribute page ");
+tributePage.setAttribute("alt", "obama-tribute-page");
 
 const clashvibes = document.getElementById("clashvibesImg");
-clashvibes.src = clashvibesPic;
-clashvibes.setAttribute("alt", "Image of clashvibes wordpress theme home page");
+clashvibes.setAttribute("alt", "clashvibes-home-page");
 
 const drumMachine = document.getElementById("drumMachineImg");
-drumMachine.src = drumMachinePic;
-drumMachine.setAttribute("alt", "Image of javascript drum machine");
+drumMachine.setAttribute("alt", "drum-machine");
 
 const reactDrumMachine = document.getElementById("reactWeatherAppImg");
-reactDrumMachine.src = reactDrumMachinePic;
-reactDrumMachine.setAttribute("alt", "Image of react weather app home page");
+reactDrumMachine.setAttribute("alt", "react-weather-app");
 
 const reactWeatherApp = document.getElementById("reactQuoteGeneratorImg");
-reactWeatherApp.src = reactWeatherAppPic;
-reactWeatherApp.setAttribute("alt", "Image of shoestore-website-home-page");
+reactWeatherApp.setAttribute("alt", "react-random-quote-generator");
+
+// get portfolio grid images
+const portImages = document.querySelectorAll("img.projectImg");
+
+// function to create srcset markup string to add to images
+function makeSrcset(imgSrc) {
+  const markup = [];
+  let width = 240;
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < 10; i++) {
+    markup[i] = `${imgSrc}.png ${width}w`;
+    width += 20;
+  }
+
+  return markup.join();
+}
+// loop over inages
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < portImages.length; i++) {
+  // get relative path of image.
+  let imgLink = portImages[i].getAttribute("src");
+  imgLink = imgLink.slice(0, -4);
+  const srcset = makeSrcset(imgLink);
+  // eslint-disable-next-line no-console
+  // console.log(srcset);
+
+  portImages[i].srcset = srcset;
+  portImages[i].sizes =
+    "(min-width: 90em) 370px, (min-width: 80em) and (max-width: 1439px) 350px, (min-width: 64em) and (max-width: 1279px) 330px, (min-width: 46em) and (max-width: 1023px) 310px, (min-width: 37.5em) and (max-width: 735px) 310px,(min-width: 30em) and (max-width: 599px) 330px, (min-width: 22.5em) and (max-width: 479px) 310px, 85vw";
+}
