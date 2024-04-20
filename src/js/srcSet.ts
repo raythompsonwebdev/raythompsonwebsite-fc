f1e9b5f1ff97a8f94f98b2f1cf1e86645bcd325e
raythompsonwebdev <raymond.thompson@raythompsonwebdev.co.uk>
2024-04-20 15:50:12 +0100

    update css - updated images srcset for responsive protfolio images

--- src/js/srcSet.ts
+++ src/js/srcSet.ts
@@ -12,11 +12,11 @@ const SrcSet = () => {
   // function to create srcset markup string to add to images
   const makeSrcset = (imgSrc: string) => {
     const markup = [];
-    let width = 300;
+    let width = 390;
 
-    for (let i = 0; i < 10; i += 1) {
-      markup[i] = `${imgSrc}.webp ${width}w `;
-      width += 20;
+    for (let i = 0; i < 4; i++) {
+      markup[i] = `${imgSrc}-${width}.webp ${width}w `;
+      width -= 20;
     }
 
     return markup.join();
@@ -24,19 +24,18 @@ const SrcSet = () => {
   //loop over inages
   //eslint-disable-next-line no-plusplus
   // for (let i = 0; i < portImagesArray.length; i += 1) {
-
   //   // get relative path of image.
-  //   let imgLink = portImagesArray[i].getAttribute("src");
+  //   let imgLink = portImagesArray[i].getAttribute("src")?.slice(0, -5) || "";
 
   //   // remove file ext
-  //   imgLink = imgLink?.slice(0, -5);
+  //   // imgLink = imgLink?.slice(0, -5);
 
-  //   const srcset = makeSrcset(imgLink) as string ;
+  //   const srcset = makeSrcset(imgLink) as string;
 
   //   portImagesArray[i].setAttribute("loading", "lazy");
   //   portImagesArray[i].srcset = srcset;
-  //   portImagesArray[i].sizes = "(width >= 1920px) 380px, (width >= 1440px) and (width <= 1919px) 380px, (width >= 1024px) and (width <= 1279px) 380px,(width >= 736px) and (width <= 1023px) 360px,(width >= 601px) and (width <= 735px) 360px,(width >= 481px) and (width <= 600px) 320px, (width >= 361px) and (width <= 480px) 320px, 100vw";
-
+  //   portImagesArray[i].sizes =
+  //     "(width >= 1920px) 390px, (width >= 1440px) and (width <= 1919px) 370px,  (width >= 1280px) and (width <= 1439px) 350px, (width >= 1024px) and (width <= 1279px) 350px, (width >= 736px) and (width <= 1023px) 330px,(width >= 601px) and (width <= 735px) 330px, (width >= 361px) and (width <= 480px) 330px, 100vw";
   // }
 
   // Loop over images using forEach
@@ -49,7 +48,7 @@ const SrcSet = () => {
     img.setAttribute("loading", "lazy");
     img.srcset = srcset;
     img.sizes =
-      "(min-width: 1920px) 390px, (min-width: 1440px) 370px,  (min-width: 1280px) 350px, (min-width: 1024px) 350px, (min-width: 736px) 330px,(min-width: 601px) 330px, (min-width: 300px) 330px, 100vw";
+      "(min-width : 1920px) 390px, (min-width : 1440px) 370px,  (min-width : 1280px) 350px, (min-width : 1024px) 350px, (min-width : 736px) 330px,(min-width : 601px) 330px, (min-width : 361px) 330px, 100vw";
   });
 };
 
